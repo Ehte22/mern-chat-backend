77461b22362adb4ba61f26af58d5bf5b532560ef
Ehte22 <syedehteshamali22@gmail.com>
2024-11-21 18:21:24 +0530

    index.js updated

--- index.js
+++ index.js
@@ -18,20 +18,15 @@ app.use(cors({
     credentials: true
 }))
 
-app.use(express.static("dist"))
 app.use(express.static("uploads"))
 app.use(express.static("profile"))
+
 app.use("/api/auth", require("./routes/auth.routes"))
 app.use("/api/chat", userProtected, require("./routes/chat.route"))
 app.use("/api/user", userProtected, require("./routes/user.route"))
 
-
-
-// app.use("*", (req, res) => {
-//     res.status(404).json({ message: "No resource found" })
-// })
 app.use("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "dist", "index.html"))
+    res.status(404).json({ message: "No resource found" })
 })
 
 app.use((err, req, res, next) => {
